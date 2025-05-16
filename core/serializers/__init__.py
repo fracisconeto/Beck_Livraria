4a0bcea5d69731443ea61347248cb56456cfaff5
fracisconeto <vargasfrancisco227@gmail.com>
2025-05-16 15:50:57 -0300

    fix: retirar trailets

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,7 +1,6 @@
-from traitlets import import_item
 from .user import UserSerializer
 from .autor import AutorSerializer
 from .editora import EditoraSerializers
 from .categoria import CategoriaSerializer
-from .livro import LivroListRetrieveSerializer,LivroSerializer
+from .livro import LivroListRetrieveSerializer, LivroSerializer
 
