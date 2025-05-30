c12146231062598c129a2f631c3151026fac3404
fracisconeto <vargasfrancisco227@gmail.com>
2025-05-30 14:05:31 -0300

    feat: adicionando Cloudinary

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,5 +2,5 @@ from .user import UserSerializer
 from .autor import AutorSerializer
 from .editora import EditoraSerializers
 from .categoria import CategoriaSerializer
-from .livro import LivroListRetrieveSerializer, LivroSerializer
+from .livro import LivroRetrieveSerializer, LivroSerializer
 
--- core/views/livro.py
+++ core/views/livro.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
-from core.serializers import LivroListRetrieveSerializer, LivroSerializer
+from core.serializers import LivroRetrieveSerializer, LivroSerializer
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
@@ -10,5 +10,5 @@ class LivroViewSet(ModelViewSet):
 
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
-            return LivroListRetrieveSerializer
+            return LivroRetrieveSerializer
         return LivroSerializer
